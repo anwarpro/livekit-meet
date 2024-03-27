a22d73da79c11de517853a3b39a4ffc33068e67f
israfil-cse <israfilhossen10750@gmail.com>
2024-03-27 11:06:43 +0600

    setup : google fonts , bootstrap , sass

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   reactStrictMode: false,
   swcMinify: false,
   productionBrowserSourceMaps: true,
