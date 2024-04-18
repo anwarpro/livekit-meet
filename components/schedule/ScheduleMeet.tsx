b6dfdd118efd5e9e670dea519af3a24a5c8e4d62
israfil-cse <israfilhossen10750@gmail.com>
2024-04-18 14:13:47 +0600

    remove : test log

--- components/schedule/ScheduleMeet.tsx
+++ components/schedule/ScheduleMeet.tsx
@@ -29,45 +29,44 @@ const ScheduleMeet = ({ fetchData, openModal, event, setSelectedEvent, selectedE
             <p className="m-0 py-2">No Upcoming Events</p>
           </div>
         ) : (
-          'best'
-        )}
-        {event.map((event: any) => (
-          <div
-            key={event._id}
-            className={`schedule-card mb-5 ${event._id === selectedEvent && 'active-event'}`}
-            style={{ cursor: 'pointer' }}
-            onClick={() => setSelectedEvent(event._id)}
-          >
-            <p className="title">{event?.title}</p>
-            <div className="d-flex justify-content-start align-items-center">
-              <div className="date-time" style={{ border: 'none' }}>
-                <Image src={clockIcon} width={24} height={24} alt="copy" className="me-2" />
-                <span>{moment(event.startTime).format('hh:mm A')}</span> - {''}
-                <span>{moment(event.endTime).format('hh:mm A')}</span>
-              </div>
-              <div className="user ms-4">
-                <div className="d-flex align-items-center">
-                  <div>
-                    <Image
-                      src={
-                        event?.hostProfile?.endsWith('profileImage.png')
-                          ? placeholder
-                          : event.hostProfile
-                      }
-                      alt="user profile"
-                      width={40}
-                      height={40}
-                    />
-                  </div>
-                  <div className="ps-3">
-                    <p className="m-0 name-text">{event?.hostName}</p>
-                    <p className="m-0 role-text">{event?.hostTeam}</p>
+          event.map((event: any) => (
+            <div
+              key={event._id}
+              className={`schedule-card mb-5 ${event._id === selectedEvent && 'active-event'}`}
+              style={{ cursor: 'pointer' }}
+              onClick={() => setSelectedEvent(event._id)}
+            >
+              <p className="title">{event?.title}</p>
+              <div className="d-flex justify-content-start align-items-center">
+                <div className="date-time" style={{ border: 'none' }}>
+                  <Image src={clockIcon} width={24} height={24} alt="copy" className="me-2" />
+                  <span>{moment(event.startTime).format('hh:mm A')}</span> - {''}
+                  <span>{moment(event.endTime).format('hh:mm A')}</span>
+                </div>
+                <div className="user ms-4">
+                  <div className="d-flex align-items-center">
+                    <div>
+                      <Image
+                        src={
+                          event?.hostProfile?.endsWith('profileImage.png')
+                            ? placeholder
+                            : event.hostProfile
+                        }
+                        alt="user profile"
+                        width={40}
+                        height={40}
+                      />
+                    </div>
+                    <div className="ps-3">
+                      <p className="m-0 name-text">{event?.hostName}</p>
+                      <p className="m-0 role-text">{event?.hostTeam}</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
 
       <ScheduleModal
