04c8184d1e51059bcd0d07aece8fcdbe145add2f
israfil-cse <israfilhossen10750@gmail.com>
2024-05-02 12:32:21 +0600

    add[manual user]: validation added

--- pages/verify/[name].tsx
+++ pages/verify/[name].tsx
@@ -2,35 +2,80 @@ import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import userService from '../../service/user/userService';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import swal from 'sweetalert';
 
 const VerifyCode: NextPage = () => {
   const router = useRouter();
   const { name } = router.query as { name: string };
   const [otp, setOtp] = useState<string>('');
+  const [error, setError] = useState<boolean>(false);
 
   const handleChange = (e: any) => {
     setOtp(e.target.value);
+    const isValid = isValidOTP(e.target.value);
+    if (!isValid) {
+      setError(true);
+    } else {
+      setError(false);
+    }
   };
 
   const handleVerify = (otp: string) => {
-    userService
-      .verifyManualUser({ email: name, otp })
-      .then((res: any) => {
-        console.log('res', res?.data?.token);
-        sessionStorage.setItem('jwt-token', `${res?.data?.token}`);
-
-        // navigation.push(`/verify/${email}`)
-      })
-      .catch((err) => console.log('err', err));
+    const isValid = isValidOTP(otp);
+    if (isValid) {
+      userService
+        .verifyManualUser({ email: name, otp })
+        .then((res: any) => {
+          sessionStorage.setItem('jwt-token', `${res?.data?.token}`);
+          router.push('/');
+        })
+        .catch((err) => {
+          console.log('err', err);
+          swal('Validation Failed', err?.response?.data?.message, 'error');
+        });
+    } else {
+      setError(true);
+    }
   };
+
+  const isValidOTP = (otp: string) => {
+    const emailRegex = /^\d{6}$/;
+    return emailRegex.test(otp);
+  };
+
   return (
-    <div className="p-5">
-      <h3>Hi, please verify using the code that was sent to your email({name})</h3>
-      <div>
-        <input name="email" onChange={(e) => handleChange(e)} type="email" />
-        <button onClick={() => handleVerify(otp)}>verify me</button>
-      </div>
-    </div>
+    <Box
+      sx={{
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      <Box sx={{ p: 5, boxShadow: '1px 1px 5px 1px grey', borderRadius: '8px' }}>
+        <Typography variant="h6" gutterBottom>
+          Please verify using the OTP that was sent to your email ({name})
+        </Typography>
+
+        <TextField
+          required
+          id="outlined-required"
+          label="6 digit OTP"
+          onChange={(e) => handleChange(e)}
+          fullWidth
+          error={error}
+          sx={{ mt: 2 }}
+        />
+        <Button
+          sx={{ mt: 2, padding: '12px 30px' }}
+          onClick={() => handleVerify(otp)}
+          variant="outlined"
+        >
+          verify me
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
