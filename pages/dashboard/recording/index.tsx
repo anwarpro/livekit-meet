b96b14f8435694329742053ffc8dc1e4888ed557
israfil-cse <israfilhossen10750@gmail.com>
2024-06-25 10:48:24 +0600

    add[recording]: show title in table

--- pages/dashboard/recording/index.tsx
+++ pages/dashboard/recording/index.tsx
@@ -2,16 +2,10 @@ import React, { ReactElement, useEffect, useState } from 'react';
 import DashboardLayout from '../../../components/layouts/DashboardLayout';
 import meetService from '../../../service/meet/meetService';
 import { useSelector } from 'react-redux';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import RecordingListTable from '../../../components/recording/RecordingListTable';
 
 const recordingFields = [
+  { id: 'title', label: 'Meeting Title' },
   { id: 'roomId', label: 'Meeting Room' },
   { id: 'egressId', label: 'Livekit Egress ID' },
   { id: 'file', label: 'file Name' },
