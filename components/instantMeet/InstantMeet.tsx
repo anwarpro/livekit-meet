b1973f2951ff1482c597e2e7e33086d0f6e54d91
israfil-cse <israfilhossen10750@gmail.com>
2024-07-11 10:34:54 +0600

    add[instant meet]: handle loading state and error msg

--- components/instantMeet/InstantMeet.tsx
+++ components/instantMeet/InstantMeet.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import meetService from '../../service/meet/meetService';
 import { useRouter } from 'next/router';
 import { setRoom } from '../../lib/Slicers/meetSlice';
@@ -9,24 +9,41 @@ import swal from 'sweetalert';
 const InstantMeet = () => {
   const dispatch = useDispatch();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   const handleInstantMeet = () => {
+    setIsLoading(true);
     meetService
       .getInstantMeet()
       .then((res: any) => {
-        dispatch(setRoom(res?.data?.data));
         router.push(`/rooms/${res?.data?.data?.roomId}`);
+        dispatch(setRoom(res?.data?.data));
+        if (res?.data?.data?.roomId && router.pathname.includes('/dashboard/instant-meet')) {
+          setIsLoading(true);
+        } else {
+          setIsLoading(false);
+        }
       })
       .catch((err) => {
-        swal('Oops...', err?.response?.data.message, 'error');
+        setIsLoading(false);
+        swal('Oops...', err?.response?.data?.message || err?.message, 'error');
         console.log('err', err);
       });
   };
   return (
     <div>
       <Box>
-      <Typography fontWeight="500" className='my-3' fontSize="18px">Instant Meet</Typography>
-        <Button onClick={() => handleInstantMeet()} variant="contained" size='large'>
-          Create Instant Meet
+        <Typography fontWeight="500" className="my-3" fontSize="18px">
+          Instant Meet
+        </Typography>
+        <Button
+          onClick={() => handleInstantMeet()}
+          variant="contained"
+          size="large"
+          disabled={isLoading}
+        >
+          {isLoading && router.pathname.includes('/dashboard/instant-meet')
+            ? 'redirecting...'
+            : 'Create Instant Meet'}
         </Button>
       </Box>
     </div>
